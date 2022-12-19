618a1a93b634621cc573baa43cac81801c3b8cc7
Artur <fowlart1988@gmail.com>
2022-12-20 01:34:13 +0200

    [19.12.2022] small fixes

--- src/main/java/com/fowlart/main/KeyboardHelper.java
+++ src/main/java/com/fowlart/main/KeyboardHelper.java
@@ -1,19 +1,13 @@
 package com.fowlart.main;
 
 import com.fowlart.main.in_mem_catalog.Catalog;
-import com.fowlart.main.in_mem_catalog.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ForceReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
-import org.telegram.telegrambots.meta.api.objects.webapp.SentWebAppMessage;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Component
 public class KeyboardHelper {
@@ -40,6 +34,7 @@ public class KeyboardHelper {
         markupInline.setKeyboard(rowsInline);
         return markupInline;
     }
+
     public InlineKeyboardMarkup buildPersonalDataEditingMenu() {
         var markupInline = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
@@ -50,21 +45,11 @@ public class KeyboardHelper {
 
         rowInline1.add(buildButton("☎️ Змінити номер телефону", "EDIT_PHONE"));
         rowInline2.add(buildButton("\uD83D\uDCCB Змінити ФІО", "EDIT_NAME"));
-        rowsInline.addAll(List.of(rowInline1,rowInline2,rowInline3));
+        rowsInline.addAll(List.of(rowInline1, rowInline2, rowInline3));
         markupInline.setKeyboard(rowsInline);
         return markupInline;
     }
-    public InlineKeyboardMarkup buildEditQtyItemMenu(Set<Item> items) {
-        var markupInline = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-        items.forEach(item -> {
-            var rowInline = new ArrayList<InlineKeyboardButton>();
-            rowInline.add(buildButton(item.id(), "QTY_" + item.id()));
-            rowsInline.add(rowInline);
-        });
-        markupInline.setKeyboard(rowsInline);
-        return markupInline;
-    }
+
     public InlineKeyboardMarkup buildMainMenuReply() {
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         List<InlineKeyboardButton> rowInline1 = new ArrayList<>();
@@ -80,40 +65,44 @@ public class KeyboardHelper {
         markupInline.setKeyboard(rowsInline);
         return markupInline;
     }
+
     public InlineKeyboardMarkup buildCatalogItemsMenu() {
 
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         List<InlineKeyboardButton> rowInline = new ArrayList<>();
         for (String groupItem : catalog.getGroupList()) {
-            rowInline.add(buildButton(groupItem, groupItem));
+            rowInline.add(buildButton("➡️ "+groupItem, groupItem));
             rowsInline.add(rowInline);
             rowInline = new ArrayList<>();
         }
         markupInline.setKeyboard(rowsInline);
         return markupInline;
     }
+
     public InlineKeyboardMarkup buildBucketItemKeyboardMenu(String itemId) {
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         List<InlineKeyboardButton> rowInline1 = new ArrayList<>();
-        rowInline1.add(buildButton("Видалити", "DISCARD_ITEM__"+itemId));
-        rowInline1.add(buildButton("Кількість", "GOODS_QTY_EDIT__"+itemId));
+        rowInline1.add(buildButton("Видалити", "DISCARD_ITEM__" + itemId));
+        rowInline1.add(buildButton("Кількість", "GOODS_QTY_EDIT__" + itemId));
         rowsInline.add(rowInline1);
         // Add it to the message
         markupInline.setKeyboard(rowsInline);
         return markupInline;
     }
+
     public InlineKeyboardMarkup buildAddToBucketItemKeyboardMenu(String itemId) {
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         List<InlineKeyboardButton> rowInline1 = new ArrayList<>();
-        rowInline1.add(buildButton("Відмінити додавання", "DISCARD_ITEM__"+itemId));
+        rowInline1.add(buildButton("Відмінити додавання", "DISCARD_ITEM__" + itemId));
         rowsInline.add(rowInline1);
         // Add it to the message
         markupInline.setKeyboard(rowsInline);
         return markupInline;
     }
+
     public InlineKeyboardMarkup buildBucketKeyboardMenu() {
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
