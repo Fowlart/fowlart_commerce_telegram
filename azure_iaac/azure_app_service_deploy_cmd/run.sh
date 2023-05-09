da796c1a8ff0c07889b97bdf3ff4540268c84dfc
Artur <fowlart1988@gmail.com>
2023-05-09 12:41:38 +0300

    [09.05.2023] added github actions: need to find a way to inject secrets in the config file

---
+++ azure_iaac/azure_app_service_deploy_cmd/run.sh
@@ -0,0 +1,13 @@
+# init
+clear
+echo "delte resource group"
+az group delete \
+  --name "tg_bot_app_service_rg" \
+  --yes
+
+echo "create resource group"
+az group create \
+  --name "tg_bot_app_service_rg" \
+  --location "polandcentral"
+
+az group list --output table
