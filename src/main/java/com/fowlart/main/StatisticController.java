fa3f3bed7f8bd94b5f7e5ef0a01afb81afad47ea
Artur <fowlart1988@gmail.com>
2023-05-21 09:17:27 +0300

    [21.05.2023] adding tracing headers

--- src/main/java/com/fowlart/main/StatisticController.java
+++ src/main/java/com/fowlart/main/StatisticController.java
@@ -6,14 +6,11 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fowlart.main.dto.BotVisitorDto;
 import com.fowlart.main.in_mem_catalog.Item;
 import com.fowlart.main.state.BotVisitorService;
-import org.apache.commons.compress.utils.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.net.http.HttpRequest;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,7 +24,15 @@ public class StatisticController {
     }
 
     @GetMapping("/all-visitors")
-    public String getExampleData() {
+    public String getAllVisitorList(HttpRequest request) {
+
+        // get headers from request
+         var headers = request.headers();
+
+         headers.map().forEach((key, value) -> {
+             System.out.println(key + ":" + value);
+         });
+
 
         final ObjectMapper mapper = new ObjectMapper()
                 .enable(SerializationFeature.INDENT_OUTPUT);
