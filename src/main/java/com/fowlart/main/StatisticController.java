0aee85ca651141db3492c22cd41b5aafdbd2968f
Artur <fowlart1988@gmail.com>
2023-05-22 16:00:28 +0300

    [22.05.2023] added admin recognition

--- src/main/java/com/fowlart/main/StatisticController.java
+++ src/main/java/com/fowlart/main/StatisticController.java
@@ -47,7 +47,7 @@ public class StatisticController {
     public String getAllVisitorList(@RequestHeader Map<String, String> headers) {
 
         var pleaseLogin = "You are not admin! Please login with Google account!" +
-                "</br> hostname/.auth/login/google/callback”>click here to login".replaceAll("hostname", hostName);
+                "</br> hostname/.auth/login/google/callback".replaceAll("hostname", hostName);
 
         // check if the request is from the admin
         var googleAccessToken = headers.get("x-ms-token-google-access-token");
