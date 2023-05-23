63e68c8e905230fc25a65b23da8aac07065d6d4f
Artur <fowlart1988@gmail.com>
2023-05-23 22:34:38 +0300

    [23.05.2023] added endpoint for receive file

--- src/main/java/com/fowlart/main/FileAccepterController.java
+++ src/main/java/com/fowlart/main/FileAccepterController.java
@@ -1,5 +1,7 @@
 package com.fowlart.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,12 @@ import java.io.IOException;
 @RestController
 public class FileAccepterController {
 
+    private final static Logger logger = LoggerFactory.getLogger(FileAccepterController.class);
     @GetMapping("/accept-file")
     public String handleFileUpload(@RequestParam("file") MultipartFile file) {
-
         var filename = file.getOriginalFilename();
         var fileInStore = new File("/botstore/received_files/"+filename);
+        logger.info("file received: {}", filename);
         try {
             file.transferTo(fileInStore);
         } catch (IOException e) {
