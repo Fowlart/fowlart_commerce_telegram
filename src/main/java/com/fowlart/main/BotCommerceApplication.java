4155537bac1e55e0654075b79c9f84335e8b8339
Artur <fowlart1988@gmail.com>
2023-08-12 01:00:36 +0300

    [12.08.2023] using Azure Monitor Application Insights with Spring Boot

--- src/main/java/com/fowlart/main/BotCommerceApplication.java
+++ src/main/java/com/fowlart/main/BotCommerceApplication.java
@@ -1,6 +1,7 @@
 package com.fowlart.main;
 
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,24 +11,25 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-    @SpringBootApplication
+@SpringBootApplication
 @Configuration
 public class BotCommerceApplication {
 
-        private final static Logger logger = LoggerFactory.getLogger(BotCommerceApplication.class);
+    private final static Logger logger = LoggerFactory.getLogger(BotCommerceApplication.class);
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        ApplicationInsights.attach();
         var activeProfile = System.getProperty("spring.profiles.active");
-        System.out.println("ACTIVE PROFILE: "+activeProfile);
+        System.out.println("ACTIVE PROFILE: " + activeProfile);
         SpringApplication.run(BotCommerceApplication.class, args);
         TelegramBotsApi telegramBotsApi;
-            logger.info("Registering bot...");
+        logger.info("Registering bot...");
         try {
             telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(Bot.getInstance());
         } catch (TelegramApiException e) {
             logger.info("Failed to register bot(check internet connection / bot token or make sure only one instance of bot is running).");
         }
-            logger.info("Telegram bot is ready to accept updates from user!");
+        logger.info("Telegram bot is ready to accept updates from user!");
     }
 }
