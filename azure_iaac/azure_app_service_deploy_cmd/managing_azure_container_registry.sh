7a56fffd1a5b477edb381d03c15819c6da94ee23
Artur <fowlart1988@gmail.com>
2023-10-02 20:37:53 +0300

    [2.10.2023] added public catalog

--- azure_iaac/azure_app_service_deploy_cmd/managing_azure_container_registry.sh
+++ azure_iaac/azure_app_service_deploy_cmd/managing_azure_container_registry.sh
@@ -16,7 +16,12 @@ az acr repository delete -n $ACR_NAME --image fowlart-tg-bot-999 --yes
 
 # deploy new image
 az webapp config container set -n $APP_NAME \
--g $RG_NAME --docker-custom-image-name $ACR_NAME.azurecr.io/fowlart-tg-bot-20231002:latest \
+-g $RG_NAME --docker-custom-image-name $ACR_NAME.azurecr.io/test_image:v1 \
  --docker-registry-server-url https://$ACR_NAME.azurecr.io \
  --docker-registry-server-user $ACR_NAME \
  --docker-registry-server-password $ACR_PASSWORD
+
+az acr build --image test_image:v1  \
+    --registry $ACR_NAME \
+    --resource-group $RG_NAME \
+    --file Dockerfile .
