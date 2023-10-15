293c4437f0a4785e59a8b2a3ddf70d5fd24a49a9
Artur <fowlart1988@gmail.com>
2023-10-15 09:43:02 +0300

    [14.10.2023] add cart in-mem handling

--- azure_iaac/azure_app_service_deploy_cmd/app_service_create_app.sh
+++ azure_iaac/azure_app_service_deploy_cmd/app_service_create_app.sh
@@ -18,15 +18,15 @@ az webapp config appsettings set --name $APP_NAME --resource-group $RG_NAME --se
 
 # create an image
 cd /Users/artur/IdeaProjects/fowlart_commerce_telegram
-az acr build --image dzmil-catalog:v2  \
+az acr build --image dzmil-catalog:v3  \
     --registry $ACR_NAME \
     --resource-group $RG_NAME \
     --file Dockerfile .
 
-# set up container
+
 az webapp config container set --name $APP_NAME --resource-group $RG_NAME \
 --resource-group $RG_NAME \
---docker-custom-image-name $ACR_NAME.azurecr.io/dzmil-catalog:v2 \
+--docker-custom-image-name $ACR_NAME.azurecr.io/dzmil-catalog:v3 \
 --docker-registry-server-url https://$ACR_NAME.azurecr.io \
 --docker-registry-server-user $ACR_NAME \
 --docker-registry-server-password $ACR_PASS
